686394ababea64315ddcc9694c1b997ccc5ee2dd
Luca Arnecke <luca@arnecke.name>
2024-10-08 00:02:39 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+
+<template>
+    <div class="h-screen flex items-center justify-center bg-gray-100">
+        <h1 class="text-4xl font-bold text-blue-500">
+            Hello, Vite + Vue + Tailwind!
+        </h1>
+    </div>
+</template>
+
+<style scoped></style>
