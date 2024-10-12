900ff491e2a8c9bdd157c0855f6f76db5cc157ea
Luca Arnecke <luca@arnecke.name>
2024-10-12 21:43:12 +0200

    store tasks in localstorage

--- src/tasks.js
+++ src/tasks.js
@@ -1,21 +1,33 @@
-import { computed, reactive } from 'vue'
+import { computed, reactive, watch } from 'vue'
+
+const storedTasks = JSON.parse(localStorage.getItem('tasks')) || []
 
 const taskData = reactive({
-    tasks: [
-        {
-            id: 1,
-            title: 'Finish application',
-            category: 'Work',
-            completed: false,
-        },
-        {
-            id: 2,
-            title: 'Go shopping',
-            category: 'Personal',
-            completed: true,
-        },
-    ],
+    tasks: storedTasks.length
+        ? storedTasks
+        : [
+              {
+                  id: 1,
+                  title: 'Finish application',
+                  category: 'Work',
+                  completed: false,
+              },
+              {
+                  id: 2,
+                  title: 'Go shopping',
+                  category: 'Personal',
+                  completed: true,
+              },
+          ],
 })
+
+watch(
+    () => taskData.tasks,
+    (newTasks) => {
+        localStorage.setItem('tasks', JSON.stringify(newTasks))
+    },
+    { deep: true }
+)
 
 const allTasks = computed(() => taskData.tasks)
 const completedTasks = computed(() =>
